a2a2c55586aa7205b17a07e073f07844fbd501d2
Adam Johnson <adamj1379@gmail.com>
2024-01-19 10:49:19 -0700

    Adding javascript file, more practice w/ html, css

---
+++ java.js
@@ -0,0 +1,7 @@
+function myFunction() {
+    document.getElementById('demo').innerHTML = 'Paragraph changed.';
+}
+
+function buttonpush() {
+    document.getElementById('buttn').innerHTML = 'wrong choice';
+}
