7c19289c7dcb16d170c5543ec4ff9f938499c21e
Adam Johnson <adamj1379@gmail.com>
2024-01-19 14:18:11 -0700

    Trying to figure out storing data on javascript

--- java.js
+++ java.js
@@ -1,32 +1,38 @@
-let credentials = [{'name':'adam', 'password':12345}]
+let credentials = [{'name':'john', 'password':54321},{'name':'adam', 'password':12345}]
+let current_credentials = []
 var signed_in = false
 
-function myFunction() {
-    document.getElementById('demo').innerHTML = 'Paragraph changed.';
-}
 
 function buttonpush() {
     document.getElementById('buttn').innerHTML = 'wrong choice';
-    document.getElementById('header_name').innerHTML = 'signed-in';
+    if (current_credentials) {
+        document.getElementById('header_name').innerHTML = current_credentials.name
+    } else {
+        document.getElementById('header_name').innerHTML = 'signed-out'
+    }
+    
 }
 
 function login() {
     const nam = document.getElementById('username').value
     const pswrd = document.getElementById('password').value
 
-    for (let i = 0; i < credentials.length; i ++) {
+    for (let i = 0; i < credentials.length; i+=1) {
         if (credentials[i].name == nam && credentials[i].password == pswrd) {
             console.log(`Welcome back, ${nam}!`)
-            console.log(credentials)
-            signed_in = true
-            document.getElementById('header_name').innerHTML = nam;
-        } else {
-            
-            console.log(`Invalid Credentials!`)
-            alert('Invalid Credentials!')
+            console.log(credentials[i])
+            console.log(current_credentials)
+            current_credentials = credentials[i]
+            console.log(current_credentials)
+            document.getElementById('header_name').innerHTML = nam
+            return
         }
-        }
-    
+    }
+    console.log(`Invalid Credentials!`);
+    alert('Invalid Credentials!');
+    return
+}        
+
 
     /*console.log(`Username: ${nam}!`)
     console.log(`Password: ${pswrd}`)
@@ -34,6 +40,6 @@ function login() {
     /*alert(credentials[0].name)
     alert(credentials[0].password)
     alert('Credentials Stored')*/
-}
+
 
 
