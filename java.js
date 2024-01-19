8bdb0a94d7600ebb68060ec3ccfcd95f6e2443f7
Adam Johnson <adamj1379@gmail.com>
2024-01-19 13:05:58 -0700

    Javascript practice, especially buttons & console

--- java.js
+++ java.js
@@ -1,14 +1,39 @@
+let credentials = [{'name':'adam', 'password':12345}]
+var signed_in = false
+
 function myFunction() {
     document.getElementById('demo').innerHTML = 'Paragraph changed.';
 }
 
 function buttonpush() {
     document.getElementById('buttn').innerHTML = 'wrong choice';
+    document.getElementById('header_name').innerHTML = 'signed-in';
 }
 
 function login() {
-    const nam = document.getElementById('name').value
-    console.log(`Hello, ${nam}!`)
-    alert(nam)
+    const nam = document.getElementById('username').value
+    const pswrd = document.getElementById('password').value
+
+    for (let i = 0; i < credentials.length; i ++) {
+        if (credentials[i].name == nam && credentials[i].password == pswrd) {
+            console.log(`Welcome back, ${nam}!`)
+            console.log(credentials)
+            signed_in = true
+            document.getElementById('header_name').innerHTML = nam;
+        } else {
+            
+            console.log(`Invalid Credentials!`)
+            alert('Invalid Credentials!')
+        }
+        }
+    
+
+    /*console.log(`Username: ${nam}!`)
+    console.log(`Password: ${pswrd}`)
+    credentials.push({'name':nam, 'password':pswrd})*/
+    /*alert(credentials[0].name)
+    alert(credentials[0].password)
+    alert('Credentials Stored')*/
 }
+
 
