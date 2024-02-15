e5453faecd782d7fb8a2ac8caf4bf7840db9e314
Adam Johnson <adamj1379@gmail.com>
2024-02-15 14:35:12 -0700

    Added JavaScript function to add comments on the main.html page. Same formatting as previous comments. It will also look at the top of the page and see the name of the user and automatically add it under the comment. Added an id for the user's name on all html pages, so it can be referenced.

--- JavaScript.js
+++ JavaScript.js
@@ -1,8 +1,18 @@
 function comment() {
-    const comment_input = document.createTextNode(document.getElementById("new_comment").value)
-    const nw_cmnt = document.createElement('div')
-    const old_comments = document.getElementById('old_comments')
-    old_comments.appendChild(nw_cmnt)
-    nw_cmnt.appendChild(comment_input)
-    
+    const neww_commment = document.getElementById("new_comment");
+    const nw_cmnt = document.createElement("div");    
+    const parag = document.createElement("p");
+    const name_span = document.createElement("span")
+    const comment_input = document.createTextNode(neww_commment.value);
+    parag.appendChild(comment_input);
+    parag.append(document.createElement("br"));
+    nw_cmnt.appendChild(parag);
+    const user_name = document.getElementById("user_name")
+    const span_txt = document.createTextNode("-"+user_name.innerText)
+    name_span.appendChild(span_txt)
+    nw_cmnt.appendChild(name_span)
+    const old_comments = document.getElementById("comments_old");
+    old_comments.appendChild(nw_cmnt);
+    old_comments.appendChild(document.createElement('hr'));
 }
+
