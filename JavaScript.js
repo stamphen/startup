5736c0fd3efce048bb97ebb47596120502bc6866
Adam Johnson <adamj1379@gmail.com>
2024-02-29 12:23:09 -0700

    JS ";" syntax and clear() func

--- JavaScript.js
+++ JavaScript.js
@@ -9,7 +9,9 @@ window.onload = names();
 //    main_pic.addEventListener("DOMContentLoaded", ev_pc())
 //}
 
-
+function clear() {
+    localStorage.clear()
+}
 
 function comment() {
     const neww_commment = document.getElementById("new_comment");
@@ -27,7 +29,7 @@ function comment() {
     const old_comments = document.getElementById("comments_old");
     old_comments.appendChild(nw_cmnt);
     old_comments.appendChild(document.createElement('hr'));
-}
+};
 
 function submit_pic() {
     const new_pic = document.getElementById("picture");
@@ -44,13 +46,13 @@ function submit_pic() {
     pic_name.appendChild(span_txt);
     const pic_url = URL.createObjectURL(new_pic.files[0]);
     pic_img.src = pic_url;  
-}
+};
 
 function login() {
     const btn = document.getElementById("login_btn");
     if (btn.hasAttribute("formaction")) {
         btn.removeAttribute("formaction");
-    }
+    };
     const username = document.getElementById("username").value;
     const pswrd = document.getElementById("password").value;
     if (username in localStorage) {
@@ -61,18 +63,18 @@ function login() {
         } else {
             console.log("Incorrect Credentials!");
             alert("Incorrect Credentials!");
-        }
+        };
     } else {
         console.log("Username not Found!");
         alert("Username not Found!");
-    }
-}
+    };
+};
 
 function create_account() {
     const btn = document.getElementById("create_btn");
     if (btn.hasAttribute("formaction")) {
         btn.removeAttribute("formaction");
-    }
+    };
     const username = document.getElementById("username").value;
     const pswrd = document.getElementById("password").value;
     if (username in localStorage) {
@@ -82,75 +84,75 @@ function create_account() {
         localStorage.setItem(username, pswrd);
         console.log("Thanks for creating an account, ",username);
         localStorage.setItem("logged-in",username);
-        const logout_btn = document.getElementById("logout_btn")
-        logout_btn.setAttribute("hidden", false)
+        const logout_btn = document.getElementById("logout_btn");
+        logout_btn.setAttribute("hidden", false);
         btn.setAttribute("formaction", "events.html");
-    }
-}
+    };
+};
 
 function logged_in_data() {
     if ("logged-in" in localStorage) {
-        console.log("Logged in!")
-        let local = document.location.href
+        console.log("Logged in!");
+        let local = document.location.href;
         if (local = "https://startup.familyjournal.click/index.html") {
             try {
-                document.getElementById("hidden_nav1").removeAttribute("hidden")
+                document.getElementById("hidden_nav1").removeAttribute("hidden");
             } catch {
             setTimeout(() => {
-                document.getElementById("hidden_nav1").removeAttribute("hidden")
+                document.getElementById("hidden_nav1").removeAttribute("hidden");
             }, 300);
-            }
-        }
-        const current_user = localStorage.getItem("logged-in")
-        console.log(current_user)
+            };
+        };
+        const current_user = localStorage.getItem("logged-in");
+        console.log(current_user);
     } else {
-        console.log("Not logged in!")
+        console.log("Not logged in!");
         try {
-            document.getElementById("hidden_nav1").setAttribute("hidden",true)
+            document.getElementById("hidden_nav1").setAttribute("hidden",true);
         } catch {
             setTimeout(() => {
-                document.getElementById("hidden_nav1").setAttribute("hidden",true)
+                document.getElementById("hidden_nav1").setAttribute("hidden",true);
             }, 300);
-        }
-        }
-    }
+        };
+        };
+    };
 
 function logout() {
-    localStorage.removeItem("logged-in")
+    localStorage.removeItem("logged-in");
 }
 
 function add_person() {
-    const new_perp = document.getElementById("members").value
-    const perp_list = document.getElementById("people_list")
+    const new_perp = document.getElementById("members").value;
+    const perp_list = document.getElementById("people_list");
     if (new_perp in localStorage) {
-        console.log("Person Successfully Added!")
-        const new_person = document.createElement("li")
-        const person_text = document.createTextNode(new_perp)
-        new_person.appendChild(person_text)
-        perp_list.appendChild(new_person)
+        console.log("Person Successfully Added!");
+        const new_person = document.createElement("li");
+        const person_text = document.createTextNode(new_perp);
+        new_person.appendChild(person_text);
+        perp_list.appendChild(new_person);
     } else {
-        console.log("No person with this username!")
-        alert('No person with this username!')
-    }
-}
+        console.log("No person with this username!");
+        alert('No person with this username!');
+    };
+};
 
 function new_event() {
-    console.log("new_event_happening")
-    const event_name = document.getElementById("event_name").value
-    const event_pic = document.getElementById("event_pic").value
-    const event_date_1 = document.getElementById("event_dates_1")
-    const event_date_2 = document.getElementById("event_dates_2")
-    const new_peeps = document.getElementById("people_list")
-    let add_members = []
+    console.log("new_event_happening");
+    const event_name = document.getElementById("event_name").value;
+    const event_pic = document.getElementById("event_pic").value;
+    const event_date_1 = document.getElementById("event_dates_1");
+    const event_date_2 = document.getElementById("event_dates_2");
+    const new_peeps = document.getElementById("people_list");
+    let add_members = [];
     for (chil of new_peeps.children) {
-        add_members.push(chil.innerText)
-    }
-    const event_data = {name:event_name, pic:event_pic, date1:event_date_1, date2:event_date_2, members:add_members}
-    localStorage.setItem(JSON.stringify(event_name), JSON.stringify(event_data))
-    console.log(localStorage.getItem(JSON.stringify(event_name)))
-    console.log(JSON.stringify(event_name))
-    localStorage.setItem("current_event",event_name)
-}
+        add_members.push(chil.innerText);
+    };
+    const event_data = {name:event_name, pic:event_pic, date1:event_date_1, date2:event_date_2, members:add_members};
+    localStorage.setItem(JSON.stringify(event_name), JSON.stringify(event_data));
+    console.log(localStorage.getItem(JSON.stringify(event_name)));
+    console.log(JSON.stringify(event_name));
+    localStorage.setItem("current_event",event_name);
+};
 
     //document.querySelector("#event_new_btn").setAttribute("formaction","main.html")
     //document.querySelector("#main_pic").src = URL.createObjectURL(event_pic.files[0])
@@ -158,31 +160,31 @@ function new_event() {
 
 
 function current_user() {
-    console.log('hi!!')
-    const display_user = document.querySelector("#user_name")
-    const curr_use = localStorage.getItem("logged-in")
-    display_user.textContent = "User: "+curr_use
+    console.log('hi!!');
+    const display_user = document.querySelector("#user_name");
+    const curr_use = localStorage.getItem("logged-in");
+    display_user.textContent = "User: "+curr_use;
 }
 
 function ev_pc() {
-    console.log(localStorage)
-    const event_pic = document.querySelector("#main_pic")
-    const event = localStorage.getItem("current_event")
-    console.log(event)
-    const event_data = JSON.parse(localStorage.getItem(JSON.stringify(event)))
-    console.log(event_data)
-    console.log("success!!!")
-    console.log(event_data.pic)
-    new_url = URL.createObjectURL(event_data.pic)
-    console.log(new_url)
-    event_pic.src = new_url
-}
+    console.log(localStorage);
+    const event_pic = document.querySelector("#main_pic");
+    const event = localStorage.getItem("current_event");
+    console.log(event);
+    const event_data = JSON.parse(localStorage.getItem(JSON.stringify(event)));
+    console.log(event_data);
+    console.log("success!!!");
+    console.log(event_data.pic);
+    new_url = URL.createObjectURL(event_data.pic);
+    console.log(new_url);
+    event_pic.src = new_url;
+};
 
 let chat = 'I loved that part of the vacation!! Honestly, we should totally do something again soon!';
 setInterval(() => {
     chat = `${chat} plus the steak was glorious...`;
     document.querySelector('#new_comment').textContent = chat;
-    comment()
+    comment();
 }, 10000);
 
 function names() {
